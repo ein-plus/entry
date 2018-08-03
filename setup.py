e538779ba3b0b3595b6f03035ca9d596a4894381
timfeirg <kkcocogogo@gmail.com>
2018-08-03 12:05:25 +0800

    go easy on websocket-client versions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    'websocket-client==0.32.0',
+    'websocket-client>=0.32.0',
     'protobuf==3.5.0.post1',
     'six>=1.9.0',
 ]
@@ -9,7 +9,7 @@ requirements = [
 setup(
     name="einplus_entryclient",
     author='EricPai <ericpai94@hotmail.com>',
-    version='2.4.0',
+    version='2.4.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
